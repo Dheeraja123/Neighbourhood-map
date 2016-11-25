0f637162cf0d1fa8475637363dda4e3f7b4318d0
Dheeraja R <dheeraja93@gmail.com>
2016-11-25 19:05:20 +0530

    Create studentdata.js

---
+++ studentdata.js
@@ -0,0 +1,52 @@
+// This JSON will be moved (once i figure out where to store it!)
+// I used several web apps to convert street address data to
+// coordinates (geocoding).
+
+var locationData = [
+{"Last":"Benevento","First":"Mark","Year":2015,"Latitude":39.31262377,"Longitude":-74.60141367,"latLng":{lat:39.3126237685211, lng:-74.6014136705948}},
+{"Last":"Hammerstedt","First":"James","Year":2015,"Latitude":39.39159404,"Longitude":-74.8186232,"latLng":{lat:39.3915940436056, lng:-74.818623197166}},
+{"Last":"Scull","First":"Brendan","Year":2015,"Latitude":39.68652842,"Longitude":-75.04706233,"latLng":{lat:39.6865284247898, lng:-75.0470623280455}},
+{"Last":"Abenante","First":"Louis","Year":2015,"Latitude":39.31288447,"Longitude":-74.53692599,"latLng":{lat:39.3128844690753, lng:-74.5369259933355}},
+{"Last":"Albrecht","First":"Will","Year":2015,"Latitude":39.13411221,"Longitude":-74.78774086,"latLng":{lat:39.1341122091274, lng:-74.7877408604455}},
+{"Last":"Aldrich","First":"Nathan","Year":2015,"Latitude":39.629479,"Longitude":-75.046477,"latLng":{lat:39.629479, lng:-75.046477}},
+{"Last":"Alvarez","First":"Daniel","Year":2015,"Latitude":39.42949286,"Longitude":-74.63782139,"latLng":{lat:39.4294928646727, lng:-74.6378213944085}},
+{"Last":"Alvarez","First":"Steven","Year":2015,"Latitude":39.42949286,"Longitude":-74.63782139,"latLng":{lat:39.4294928646727, lng:-74.6378213944085}},
+{"Last":"Alvino ","First":"Ernest","Year":2015,"Latitude":39.74473708,"Longitude":-75.24532748,"latLng":{lat:39.7447370819354, lng:-75.2453274789433}},
+{"Last":"Ammirato","First":"Joshua","Year":2015,"Latitude":39.836371,"Longitude":-75.181675,"latLng":{lat:39.836371, lng:-75.181675}},
+{"Last":"Antonucci","First":"Haddon","Year":2015,"Latitude":39.42938014,"Longitude":-74.71377967,"latLng":{lat:39.4293801375825, lng:-74.7137796680984}},
+{"Last":"Arena","First":"Alec","Year":2015,"Latitude":39.63596421,"Longitude":-74.78275914,"latLng":{lat:39.6359642130797, lng:-74.7827591398402}},
+{"Last":"Augusto","First":"Nicolas","Year":2015,"Latitude":39.42895954,"Longitude":-74.61665688,"latLng":{lat:39.4289595428059, lng:-74.616656879828}},
+{"Last":"Azarian","First":"Chase","Year":2015,"Latitude":39.73092343,"Longitude":-75.20671772,"latLng":{lat:39.7309234265242, lng:-75.2067177186023}},
+{"Last":"Bailey","First":"William","Year":2015,"Latitude":39.25554,"Longitude":-74.80704,"latLng":{lat:39.25554, lng:-74.80704}},
+{"Last":"Batchelor","First":"Justin","Year":2015,"Latitude":39.6335727,"Longitude":-75.04933309,"latLng":{lat:39.6335726973557, lng:-75.0493330880468}},
+{"Last":"Battschinger","First":"Gregory","Year":2015,"Latitude":39.52728,"Longitude":-74.6067,"latLng":{lat:39.52728, lng:-74.6067}},
+{"Last":"Baylinson","First":"Jackson","Year":2015,"Latitude":39.3353411,"Longitude":-74.5086814,"latLng":{lat:39.335341102101, lng:-74.5086814016552}},
+{"Last":"Beecroft","First":"Richard","Year":2015,"Latitude":39.4497521,"Longitude":-75.20582505,"latLng":{lat:39.4497520968028, lng:-75.2058250501777}},
+{"Last":"Benevento","First":"Mark","Year":2015,"Latitude":39.30755192,"Longitude":-74.6032376,"latLng":{lat:39.3075519216031, lng:-74.6032375972081}},
+{"Last":"Bloh","First":"Andrew","Year":2015,"Latitude":39.34865762,"Longitude":-74.57451543,"latLng":{lat:39.3486576151695, lng:-74.5745154326122}},
+{"Last":"Bloomer","First":"Cole","Year":2015,"Latitude":39.48205797,"Longitude":-74.88204987,"latLng":{lat:39.4820579655104, lng:-74.8820498694684}},
+{"Last":"Bonerbo","First":"Michael","Year":2015,"Latitude":39.71404891,"Longitude":-75.0742125,"latLng":{lat:39.7140489120237, lng:-75.0742124998398}},
+{"Last":"Boyd","First":"Stephen","Year":2015,"Latitude":39.64284,"Longitude":-74.95554,"latLng":{lat:39.64284, lng:-74.95554}},
+{"Last":"Brown","First":"Ryan","Year":2015,"Latitude":39.76995046,"Longitude":-75.17229503,"latLng":{lat:39.7699504635764, lng:-75.1722950254415}},
+{"Last":"Bryant","First":"Dwayne","Year":2015,"Latitude":39.47296674,"Longitude":-75.1329743,"latLng":{lat:39.4729667435849, lng:-75.1329743019434}},
+{"Last":"Buchanan","First":"Timothy","Year":2015,"Latitude":39.08803938,"Longitude":-74.84230101,"latLng":{lat:39.0880393845887, lng:-74.8423010060561}},
+{"Last":"Buirch","First":"Warren","Year":2015,"Latitude":39.76408722,"Longitude":-75.06472363,"latLng":{lat:39.7640872175051, lng:-75.0647236256399}},
+{"Last":"Byrd","First":"Darian","Year":2015,"Latitude":39.37851146,"Longitude":-74.68088841,"latLng":{lat:39.3785114628857, lng:-74.6808884086582}},
+{"Last":"Cademartori","First":"Mathew","Year":2015,"Latitude":39.32179547,"Longitude":-74.61066557,"latLng":{lat:39.3217954658614, lng:-74.6106655666204}},
+{"Last":"Candelore","First":"Zachary","Year":2015,"Latitude":39.64284,"Longitude":-74.95554,"latLng":{lat:39.64284, lng:-74.95554}},
+{"Last":"Capille","First":"Jonathan","Year":2015,"Latitude":39.35760081,"Longitude":-74.64928355,"latLng":{lat:39.3576008065407, lng:-74.6492835495108}},
+{"Last":"Carew","First":"Sean","Year":2015,"Latitude":39.36694498,"Longitude":-74.54892779,"latLng":{lat:39.3669449844282, lng:-74.5489277927683}},
+{"Last":"Carey","First":"Mark","Year":2015,"Latitude":39.71850878,"Longitude":-75.04614749,"latLng":{lat:39.7185087814933, lng:-75.0461474896599}},
+{"Last":"Carto","First":"Christian","Year":2015,"Latitude":39.79689055,"Longitude":-75.08266498,"latLng":{lat:39.7968905475632, lng:-75.082664981238}},
+{"Last":"Castellano","First":"Steven","Year":2015,"Latitude":39.76340331,"Longitude":-75.08940842,"latLng":{lat:39.7634033112764, lng:-75.0894084154211}},
+{"Last":"Champoux","First":"Sergio","Year":2015,"Latitude":39.78737099,"Longitude":-74.93517779,"latLng":{lat:39.7873709855563, lng:-74.9351777933194}},
+{"Last":"Chapman","First":"Philo Caleb","Year":2015,"Latitude":39.52041251,"Longitude":-75.03493567,"latLng":{lat:39.5204125061204, lng:-75.034935672921}},
+{"Last":"Chell","First":"Alex ","Year":2015,"Latitude":39.76079414,"Longitude":-75.14639439,"latLng":{lat:39.7607941432397, lng:-75.1463943892459}},
+{"Last":"Cirillo","First":"Brandon","Year":2015,"Latitude":39.07690516,"Longitude":-74.81555591,"latLng":{lat:39.0769051641928, lng:-74.8155559131811}},
+{"Last":"Clark","First":"Alexander","Year":2015,"Latitude":39.37065067,"Longitude":-74.5601701,"latLng":{lat:39.3706506668399, lng:-74.5601701033264}},
+{"Last":"Clark","First":"Benjamin","Year":2015,"Latitude":39.37065067,"Longitude":-74.5601701,"latLng":{lat:39.3706506668399, lng:-74.5601701033264}},
+{"Last":"Cliver","First":"Ryan","Year":2015,"Latitude":39.34249474,"Longitude":-74.59653228,"latLng":{lat:39.342494735063, lng:-74.5965322775517}},
+{"Last":"Cohn","First":"Chase","Year":2015,"Latitude":39.49104177,"Longitude":-75.12573543,"latLng":{lat:39.4910417725569, lng:-75.1257354252569}}
+
+
+];
